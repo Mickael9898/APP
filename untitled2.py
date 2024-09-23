33922707309bb8381edd32f126b98d1751692e1f
Mickael9898 <mickael_abboud@hotmail.com>
2024-09-23 21:08:36 +0300

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -130,7 +130,7 @@ st.plotly_chart(fig_bar)
 st.subheader("Education Levels in Baabda and Beit Meri")
 
 # Define the two towns to toggle between
-toggle_towns = ['Tyre', 'Beit Meri']
+toggle_towns = ['Aley', 'Beit Meri']
 
 # Add a selectbox to switch between the two towns
 selected_town = st.radio("Select a town", toggle_towns)
